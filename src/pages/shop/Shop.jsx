3fc9b5c1aa0a675c841e1fb945c9d9cf785b6766
iamankit04 <ankitforcoding@gmail.com>
2026-06-26 01:43:13 +0530

    Ecommerce website using react

---
+++ src/pages/shop/Shop.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import "./Shop.css"
+import { category } from '../../Category'
+import { dummydata } from '../../dummydata'
+import Product from '../../components/Product/Product'
+import { FaShopify } from "react-icons/fa";
+
+function Shop() {
+    let [cate , setCate] = useState(dummydata)
+
+    function filterProducts(category){
+
+      if(category === "All"){
+         setCate(dummydata)
+      }else{
+      const updatedata = dummydata.filter((item) => (item.category === category))
+      setCate(updatedata)
+      }
+    }
+  return (
+    <div className='shop'>
+        <div className="heading">
+            <span>Shop</span>
+            <FaShopify />
+        </div>
+         <div className="category-section" >
+        {category.map((item) =>(
+            
+            <div className="category-card"onClick={() => {
+              filterProducts(item.name)
+            }}>
+             
+             <img src = {item.image} alt = "" />
+             <span>{item.name}</span>
+
+            </div>
+
+        ))}
+        </div>
+
+        <div className="product-section">
+         
+        {cate.map((item) =>(
+
+          <Product name = {item.name}  price = {item.price} image={item.image} id={item.id}   />
+        ))}
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Shop
---
+++ src/redux/store.js
@@ -0,0 +1,7 @@
+import {configureStore} from "@reduxjs/toolkit"
+import cartReducer from "./cardSlice"
+export const store = configureStore({
+    reducer:{
+        cart : cartReducer
+    }
+})
